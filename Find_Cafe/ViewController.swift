477ff01e406fb46ca65689354a38558dabb6050a
SeanYin <kimiyin@gmail.com>
2016-12-28 22:35:24 +0800

    Initial Commit

---
+++ Find_Cafe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find_Cafe
+//
+//  Created by Sean on 2016/12/28.
+//  Copyright © 2016年 Chien Hsiang Yin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
