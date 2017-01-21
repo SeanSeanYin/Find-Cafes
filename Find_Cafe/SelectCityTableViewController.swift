1a666323777f5c630f43b4deba5041b73e93808b
sean@vcube.tw <sean@vcube.tw>
2017-01-21 17:34:30 +0800

    1. Select city page is ok

--- Find_Cafe/SelectCityTableViewController.swift
+++ Find_Cafe/SelectCityTableViewController.swift
@@ -41,60 +41,59 @@ class SelectCityTableViewController: UITableViewController {
         
         return (UIScreen.main.bounds.height *  0.195)
     }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        var city = ""
+        switch (indexPath.row){
+            case 0:
+                city = "Taipei"
+            case 1:
+                city = "Hsinchu"
+            case 2:
+                city = "Taichung"
+            case 3:
+                city = "Tainan"
+            case 4:
+                city = "Kaohsiung"
+            default:
+                city = "Taipei"
+        }
+        
+        let alert = UIAlertController(title: "You select \(city) city", message: "Are you sure?", preferredStyle: .alert)
+        let doAction = UIAlertAction(title: "Yes", style: .default) { action in
+            self.performSegue(withIdentifier: "chooseCityToSearch", sender: city)
+        }
+        let cancelAction = UIAlertAction(title: "No", style: .cancel, handler: nil)
+        
+        alert.addAction(doAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+    }
     
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        var city = ""
+        
+        if (segue.identifier == "chooseCityToSearch") {
+            
+            let navigationController = segue.destination as! UINavigationController
+            let viewController = navigationController.topViewController as! CafesViewController
+            switch (sender as! String) {
+            case "Taipei":
+                city = "台北"
+            case "Hsinchu":
+                city = "新竹"
+            case "Taichung":
+                city = "台中"
+            case "Tainan":
+                city = "台南"
+            case "Kaohsiung":
+                city = "高雄"
+            default:
+                city = "台北"
+            }
+            viewController.newCity = city
+        }
     }
-    */
-
 }
